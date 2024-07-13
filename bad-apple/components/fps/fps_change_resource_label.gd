98729bc274a3a4898a67fcb20bb97e2dd056e5dc
OliPohl <oliver@pohl66.de>
2024-07-13 15:50:52 +0200

    made resource manager an autoload

--- bad-apple/components/fps/fps_change_resource_label.gd
+++ bad-apple/components/fps/fps_change_resource_label.gd
@@ -15,25 +15,19 @@ extends Node
 @export var inactive_theme : LabelSettings = null
 
 var _label : Label
-var _resource_manager : FpsResourceManager
 
 func _ready():
 	_label = get_parent()
-	_resource_manager = get_node("%FpsResourceManager")
 
-	if _resource_manager == null:
-		assert(false, str(name) + " has to have a FpsResourceManager in scene. Path: " + str(get_path()))
-		return
+	FpsResourceManagerInstance.weapon_changed.connect(_on_weapon_changed)
+	FpsResourceManagerInstance.ammo_changed.connect(_on_ammo_changed)
+	FpsResourceManagerInstance.health_changed.connect(_on_health_changed)
+	FpsResourceManagerInstance.armor_changed.connect(_on_armor_changed)
 
-	_resource_manager.weapon_changed.connect(_on_weapon_changed)
-	_resource_manager.ammo_changed.connect(_on_ammo_changed)
-	_resource_manager.health_changed.connect(_on_health_changed)
-	_resource_manager.armor_changed.connect(_on_armor_changed)
-
-	_on_weapon_changed(_resource_manager.weapon)
-	_on_ammo_changed(_resource_manager.ammo)
-	_on_health_changed(_resource_manager.health)
-	_on_armor_changed(_resource_manager.armor)
+	_on_weapon_changed(FpsResourceManagerInstance.weapon)
+	_on_ammo_changed(FpsResourceManagerInstance.ammo)
+	_on_health_changed(FpsResourceManagerInstance.health)
+	_on_armor_changed(FpsResourceManagerInstance.armor)
 
 
 func _on_weapon_changed(weapon : FpsResourceManager.WEAPOND_TYPE) -> void:
--- bad-apple/components/fps/fps_weapon_changer.gd
+++ bad-apple/components/fps/fps_weapon_changer.gd
@@ -1,20 +1,15 @@
 class_name FpsWeaponChanger
 extends Node
 
-var _resource_manager : FpsResourceManager
 var _next_time : float = 0
-
-func _ready():
-	_resource_manager = get_node("%FpsResourceManager")
-
 
 func _unhandled_input(_event):
 	if Input.is_action_just_pressed("mmb") and _next_time < Time.get_ticks_msec():
 		_next_time = Time.get_ticks_msec() + 100
-		var current_weapon = _resource_manager.get("weapon")
+		var current_weapon = FpsResourceManagerInstance.get("weapon")
 
 		match current_weapon:
 			FpsResourceManager.WEAPOND_TYPE.SHOTGUN:
-				_resource_manager.set("weapon", FpsResourceManager.WEAPOND_TYPE.KNIFE)
+				FpsResourceManagerInstance.set("weapon", FpsResourceManager.WEAPOND_TYPE.KNIFE)
 			FpsResourceManager.WEAPOND_TYPE.KNIFE:
-				_resource_manager.set("weapon", FpsResourceManager.WEAPOND_TYPE.SHOTGUN)
+				FpsResourceManagerInstance.set("weapon", FpsResourceManager.WEAPOND_TYPE.SHOTGUN)
