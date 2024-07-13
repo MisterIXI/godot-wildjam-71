75ca53d5737e2cbd041f1aae77eb21b9e955a0ec
OliPohl <oliver@pohl66.de>
2024-07-13 12:43:16 +0200

    controller feels good

--- bad-apple/scenes/fps/character/fps_controller.gd
+++ bad-apple/scenes/fps/character/fps_controller.gd
@@ -4,14 +4,16 @@ extends CharacterBody3D
 @export_range(1,15) var movement_speed :float = 8.0
 @export_range(1,15) var movement_runon : float = 3
 @export_range(1,15) var movement_falloff : float = 0.3
-@export_range(1,15) var jump_velocity : float = 4
-@export_range(1,50) var gravity : float = 10.0
+@export_range(1,15) var jump_velocity : float = 5
+@export_range(1,50) var gravity : float = 15.0
 
 @export_group("Direction")
 @export_range(1,100) var mouse_sensitivity : float = 50
 @export var clamp_angle : Vector2 = Vector2(-45, 45)
 
 var _camera : Camera3D
+var vel_x : float = 0
+var vel_z : float = 0
 
 func _ready():
 	for child in get_children():
@@ -32,24 +34,23 @@ func _handle_movement(delta : float) -> void:
 	if Input.is_action_just_pressed("space") and is_on_floor():
 		velocity.y = jump_velocity
 
-	var input_dir = Input.get_vector("left", "right", "up", "down")
-	var direction = (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
-	
+	var input = Input.get_vector("left", "right", "up", "down")
 
 	var max_speed = movement_speed
 	if Input.is_action_pressed("shift"):
 		max_speed /= 2
 
-	if direction.x:
-		velocity.x = move_toward(velocity.x, direction.x * max_speed, movement_runon)
+	if input.x:
+		vel_x = move_toward(vel_x, input.x * max_speed, movement_runon)
 	else:
-		velocity.x = move_toward(velocity.x, 0, movement_falloff)
-	
-	if direction.z:
-		velocity.z = move_toward(velocity.z, direction.z * max_speed, movement_runon)
-	else:
-		velocity.z = move_toward(velocity.z, 0, movement_falloff)
+		vel_x = move_toward(vel_x, 0, movement_falloff)
 
+	if input.y:
+		vel_z = move_toward(vel_z, input.y * max_speed, movement_runon)
+	else:
+		vel_z = move_toward(vel_z, 0, movement_falloff)
+
+	velocity = transform.basis * Vector3(vel_x, velocity.y, vel_z)
 	move_and_slide()
 
 
