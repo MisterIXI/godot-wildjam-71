fed49cd93c85c6d8d8b9ab94ca6dc584492d81b9
Yannik Braendle <yannik.braendle@t-online.de>
2024-07-14 12:55:59 +0200

    snake random spawn added

--- bad-apple/components/snake/snake_spawner.gd
+++ bad-apple/components/snake/snake_spawner.gd
@@ -7,16 +7,37 @@ class_name SnakeSpawner
 var spawned_snakes: Array[SnakePart] = []
 
 ## first and last positions are head and tail directions and will not spawn a body part
-@export var snake_positions: Array[Vector2i] = [
-	Vector2i(6,8),
-	Vector2i(7,8),
-	Vector2i(8,8),
-	Vector2i(9,8),
-	Vector2i(10,8),
-	Vector2i(11,8),
-]
+# @export var snake_positions: Array[Vector2i] = [
+# 	Vector2i(6,8),
+# 	Vector2i(7,8),
+# 	Vector2i(8,8),
+# 	Vector2i(9,8),
+# 	Vector2i(10,8),
+# 	Vector2i(11,8),
+# ]
+
+
+func random_start_position(body_parts: int = 3) -> Array[Vector2i]:
+	assert(body_parts > 0)
+	var positions : Array[Vector2i] = []
+	var initial_pos = Vector2i(randi_range(6,9), randi_range(6,9))
+	positions.append(initial_pos)
+	var direction = Vector2i(0,0)
+	match randi_range(0,3):
+		0:
+			direction = Vector2i(1,0)
+		1:
+			direction = Vector2i(-1,0)
+		2:
+			direction = Vector2i(0,1)
+		3:	
+			direction = Vector2i(0,-1)
+	for i in range(1, body_parts + 3):
+		positions.append(initial_pos + direction * i)
+	return positions
 
 func spawn_snake():
+	var snake_positions = random_start_position(5)
 	var prev_part: SnakePart = null
 	for i in snake_positions.size():
 		if i == 0 or i == snake_positions.size() - 1:
