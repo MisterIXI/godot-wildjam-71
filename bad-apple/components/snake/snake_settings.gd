bdfd38d4bdc99d5776ed8de0d9a6ada63a5d03a0
Yannik Braendle <yannik.braendle@t-online.de>
2024-07-14 16:28:19 +0200

    snake setting default change

--- bad-apple/components/snake/snake_settings.gd
+++ bad-apple/components/snake/snake_settings.gd
@@ -6,7 +6,7 @@ class_name SnakeSettings
 ## base speed in grid_tiles per second
 @export var base_speed: float = 1
 ## turn speed in radians per second
-@export_range(0.0,10.0) var turn_speed: float = 270
+@export var turn_speed: float = 8.143
 
 @export_subgroup("GridSettings")
 @export var grid_dims: Vector2i = Vector2i(16,16)
