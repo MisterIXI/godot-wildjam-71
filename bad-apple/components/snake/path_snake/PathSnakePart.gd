bc6c289bab2df5d9f3f24fe7121b11fbd844c3d2
Yannik Braendle <yannik.braendle@t-online.de>
2024-07-19 22:48:48 +0200

    menu snake paths added

---
+++ bad-apple/components/snake/path_snake/PathSnakePart.gd
@@ -0,0 +1,25 @@
+extends Area3D
+class_name PathSnakePart
+
+@export var Head: Node3D
+@export var Body: Node3D
+@export var Tail: Node3D
+@export var animation_player: AnimationPlayer
+
+func play_animation(_anim_name: String):
+	animation_player.play(_anim_name)
+
+func switch_to_head():
+	Head.visible = true
+	Body.visible = false
+	Tail.visible = false
+
+func switch_to_body():
+	Head.visible = false
+	Body.visible = true
+	Tail.visible = false
+
+func switch_to_tail():
+	Head.visible = false
+	Body.visible = false
+	Tail.visible = true
