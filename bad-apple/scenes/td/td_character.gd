a9eda0e52ddca77d9c16fee3ba210d0e5a07be8c
OliPohl <oliver@pohl66.de>
2024-07-21 15:36:40 +0200

    better controller but still shit

--- bad-apple/scenes/td/td_character.gd
+++ bad-apple/scenes/td/td_character.gd
@@ -4,10 +4,12 @@ extends CharacterBody3D
 
 const SPEED = 5.0
 const JUMP_VELOCITY = 4.5
+var last_mouse_pos = Vector2(0,0)
+var mouse_position_3D = Vector3(0,0,0)
 
 
 func _ready():
-	Input.mouse_mode = Input.MOUSE_MODE_CONFINED
+	Input.mouse_mode = Input.MOUSE_MODE_VISIBLE
 
 
 func _physics_process(_delta):
@@ -30,12 +32,26 @@ func _handle_movement():
 
 
 func _handle_rotation():
-	pass
+	var viewport := get_viewport()
+	var _mouse_position := viewport.get_mouse_position()
+	var _camera := viewport.get_camera_3d()
+	var origin := _camera.project_ray_origin(_mouse_position)
+	var direction := _camera.project_ray_normal(_mouse_position)
+	var ray_length := camera.far
+	var end := origin + direction * ray_length
+	var space_state := get_world_3d().direct_space_state
+	var query := PhysicsRayQueryParameters3D.create(origin, end)
+	var result := space_state.intersect_ray(query)
+	if not result.is_empty():
+		mouse_position_3D = result["position"]
+	print(mouse_position_3D)
+
+
+	look_at(mouse_position_3D, Vector3.UP)
+	rotation.x = 0
+	rotation.z = 0
 
 
 func _input(event):
 	if event is InputEventMouseMotion:
-		print(event.position)
-		look_at(camera.project_position(event.position, 1))
-		rotation.x = 0
-		rotation.z = 0
+		last_mouse_pos = event.position
